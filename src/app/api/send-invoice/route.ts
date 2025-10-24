a9c9df5a91523507e6f012e57254dbdbd92743ff
Past Flex <past6867@gmail.com>
2025-10-24 05:38:06 +0000

    ese cambio en la firma cambia en todos los archivos que tengan esa firma

--- src/app/api/send-invoice/route.ts
+++ src/app/api/send-invoice/route.ts
@@ -1,8 +1,6 @@
 
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-import * as fs from 'fs';
-import * as path from 'path';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
